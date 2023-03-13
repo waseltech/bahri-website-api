1bf5f3019686d43ff6fe0746f3b38f7a3c0434fd
yaseenomer <yaseeno373@gmail.com>
2023-03-14 01:05:48 +0300

    connect to atlas

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import mongoose from "mongoose";
 import { app, port } from "./app";
 import "express-async-errors";
 
+const dbUser = "yaseenomer";
+const dbPass = "oqwB6M37QrPPLCsr";
 
-
- (async () => {
+// const mongoDbUrl = `mongodb+srv://${dbUser}:${dbPass}@cluster0.k23nnlx.mongodb.net/?retryWrites=true&w=majority`;
+const mongoDbUrl = `mongodb+srv://${dbUser}:${dbPass}@yaseen-cluster.xotylcw.mongodb.net/?retryWrites=true&w=majority`;
+(async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/website");
+    await mongoose.connect(mongoDbUrl);
     console.log("connecting success to mongo db");
     app.listen(port, () => {
       console.log(`category-api listening at http://localhost:${port}`);
@@ -14,4 +17,4 @@ import "express-async-errors";
   } catch (error: any) {
     throw new Error(error);
   }
-})()
+})();
